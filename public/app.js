16d42159bac2251a0c012f9e9f322473af5a04ba
jeromeall <jeromeallouche01@gmail.com>
2014-03-08 16:17:53 -0800

    search bar working

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+
 window.App = {
   Models: {},
   Collections: {},
@@ -10,31 +11,47 @@ window.App = {
     App.autocompleter = new Autocompleter();
     var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
     ws.onmessage = function(m) { 
-      autocompleter.add(m.data); 
+      App.autocompleter.add(m.data); 
     };
 
   }
 };
+
 $(document).ready(function(){
   App.initialize();
 });
 
 
-App.Routers.Main = Backbone.Routers.Extend({
+App.Routers.Main = Backbone.Router.extend({
   routes: {
     "": "index",
   },
   index: function(){
-    var view = new App.Views.Index();
+    var view = new App.Views.Search();
     $("#container").html(view.render().el);
   }
 });
 
 App.Views.Search = Backbone.View.extend({
   id: "search_bar",
-
+  events: {
+    "keypress search_bar" : "searchFunction"
+  },
+  template: function(){ return "<form> <input type='text' id='search_bar'></form>";},
   render: function(){
-    $(this.el).append();
+    this.$el.html(this.template());
+    return this;
+  },
+  searchFunction: function(){
+
   }
 
+});
+
+
+
+
+App.Views.Results = Backbone.View.extend({
+  id: "results",
+  template: function(){return "<>";}
 });
